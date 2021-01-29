addab226777ac07ecd26d155bb1626071183bbef
rolanduwxcc <rolanduwxcc@gmail.com>
2021-01-29 17:11:11 -0600

    updated index.html side 2 made JS function to show news links

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,7 +67,10 @@ function fetchNewYorkTimesDataAbout(searchTerm) {
         searchTerm="Avengers";
     }
     let apikey="2BRTvk2fDdRpikdcvLeUnTV3zBuddquD";
-    let apiUrl="https://api.nytimes.com/svc/search/v2/articlesearch.json?q=Evans,Chris&api-key="+apikey;
+    let apiUrl="https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
+        searchTerm +
+        "&api-key=" +
+        apikey;
 
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
@@ -75,6 +78,7 @@ function fetchNewYorkTimesDataAbout(searchTerm) {
                 // console.log(nytDataJSON);
                 //INSERT FUNCTION CALL HERE MAYBE
                 //call the news building html display tag and pass nytDataJSON
+                buildNewsHTML(nytDataJSON);
             });
         }
         else {
@@ -86,7 +90,41 @@ function fetchNewYorkTimesDataAbout(searchTerm) {
     });
 }
 
+function buildNewsHTML(nytDataJSON) {
+    let articlesArray=nytDataJSON.response.docs;
+    let article_url;
+    let article_text;
+    let imageUrlPrefix = "https://www.nytimes.com/";
+    
+    let newsLinksEl=document.getElementById("news-links");
+    
+    for (let i = 0; i < 5; i++) {
+        const article = articlesArray[i];
+        
+        let newsCardEl=document.createElement("div");
+        newsCardEl.setAttribute("class","card");
+        
+        // let imageEl=document.createElement("img");
+        // imageEl.setAttribute("src",imageUrlPrefix + article.multimedia[0].url);
+        // imageEl.setAttribute("alt","thumbnail");
+        // newsCardEl.appendChild(imageEl);
+
+        //<a href="url">link text</a>
+        let articleLinkEl=document.createElement("a");
+        articleLinkEl.setAttribute("href",article.web_url);
+        articleLinkEl.textContent = article.headline.main;
+        newsCardEl.appendChild(articleLinkEl);
+
+        newsLinksEl.appendChild(newsCardEl);
+
+    }
+
+    
+
+}
 
 
 
 //-------------------------------------------LISTENERS and CALLS
+fetchMovieDataAbout();
+fetchNewYorkTimesDataAbout();
