5a30506812e60bfcd33db7d986ed3ee5533a73b1
rolanduwxcc <rolanduwxcc@gmail.com>
2021-01-29 11:34:00 -0600

    add news fetch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,30 +8,59 @@
 
 
 //-----------------------------------------------FUNCTIONS
-function getMovieDate(searchTerm) {
+function fetchMovieDataAbout(searchTerm) {
     if (!searchTerm) {
         searchTerm="Avengers";
     }
-    var apiKey="1d01c961";
-    var apiUrl="http://www.omdbapi.com/?apikey="+apiKey+"&t="+searchTerm;
+    let apiKey="1d01c961x";
+    let apiUrl="http://www.omdbapi.com/?apikey="+apiKey+"&t="+searchTerm;
 
-    fetch(apiUrl)
-    .then(function(response) {
+    fetch(apiUrl).then(function(response) {
         if(response.ok) {
             response.json().then(function(movieDataJSON){
-                console.log(movieDataJSON);
-        } 
+                //console.log(movieDataJSON);
+                //INSERT FUNCTION CALL HERE
+                //call the movie html disply tag and pass JSON
+            });
+        }
         else {
             alert("Error: " + response.statusText);            
         }
     })
     .catch(function(error) {
-        alert("Unable to connect to OpenWeather!");
+        alert("Unable to connect to OMDB!");
     });
-
 }
 
+function fetchNewsDataAbout(searchTerm) {
+    if (!searchTerm) {
+        searchTerm="Avengers";
+    }
+    let apikey="d5cbca68e3a4433cb167b442f2efa9da";
+    let url = 'http://newsapi.org/v2/everything?' +
+          'q=Apple&' +
+          'from=2021-01-29&' +
+          'sortBy=popularity&' +
+          'apiKey=' + apikey;
 
+    let req = new Request(url);
+
+    fetch(req).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(newsDataJSON) {
+                console.log(newsDataJSON);
+                //INSER FUNCTION CALL HERE MAYBE
+                //call the news html display tag and pass JSON
+            });    
+        }
+        else {
+            alert("Error: " + response.statusText);
+        }
+    })
+    .catch(function(error) {
+        alert("Unable to connect to NewsAPI!");
+    });
+}
 
 
 
